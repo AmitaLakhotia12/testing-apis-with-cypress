b318df0cf545a92b5d866f8c59b81def0ab8de25
AmitaLakhotia12 <110840717+AmitaLakhotia12@users.noreply.github.com>
2022-09-06 12:11:47 +0200

    Update spec.cy.js

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,33 @@
-describe('empty spec', () => {
+const request = require('Cypress')('https://www.rijksmuseum.nl/api/en/');
+
+const CODE_OK = 200
+const CODE_UNAUTHORIZED = 401
+const TEXT_UNAUTHORIZED = 'Invalid key'
+
+const API_KEY = '0fiuZFh4'
+
+const INVOLVED_MAKER = 'Rembrandt van Rijn'
+const ELEMENTS_NUMBER = 3608
+const ART_OBJECT_NUMBER = 'SK-C-5'
+
+const DEFAULT_NUMBER_OF_RESULTS_PER_PAGE = 10
+const NUMBER_OF_RESULTS_PER_PAGE = 1
+const PAGE_NUMBER = 1
+
+describe('visit rijksmuseum website', () => {
   it('passes', () => {
     cy.visit('https://www.rijksmuseum.nl/nl/rijksstudio')
   })
 })
+
+describe('Rijksmuseum API `GET /collection`', () => {
+
+    it('WHEN no api key THEN code IS ' + CODE_UNAUTHORIZED, () => {
+		return request
+			.get('/collection')
+			.query(
+                {   
+                    involvedMaker: INVOLVED_MAKER
+                })
+			.expect(CODE_UNAUTHORIZED)
+	});
