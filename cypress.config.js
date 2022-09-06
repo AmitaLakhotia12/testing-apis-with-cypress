91a96c39d8d10f1024d3c75a42f54c4f5eb897f4
AmitaLakhotia12 <110840717+AmitaLakhotia12@users.noreply.github.com>
2022-09-06 10:54:29 +0200

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
    // supportFile: false,
-   etupNodeEvents(on, config) {
+   SetupNodeEvents(on, config) {
       // implement node event listeners here
     },
     },
